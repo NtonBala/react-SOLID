9c6f5f4b236731d62d6a048b916e7b1000e03ae1
Anton Balashov <balashov.anton.1983@gmail.com>
2024-02-05 00:10:15 +0200

    [SRP] Decompose VideoPreview component

--- src/App.tsx
+++ src/App.tsx
@@ -30,24 +30,40 @@ const useVideoDetails = (videoId: string) => {
   return videoDetails;
 };
 
+const VideoPreviewImage = ({
+  videoDetails,
+}: {
+  videoDetails: VideoDetails;
+}) => (
+  <img
+    style={{ width: "200px", borderRadius: "10px", border: "1px solid" }}
+    src={videoDetails.previewUrl}
+    alt="video preview"
+  />
+);
+
+const VideoDescription = ({ videoDetails }: { videoDetails: VideoDetails }) => (
+  <>
+    <div style={{ fontWeight: "bold" }}>{videoDetails.title}</div>
+    <div style={{ color: "#808080" }}>{videoDetails.author}</div>
+  </>
+);
+
+const Loader = () => <span>loading...</span>;
+
 const VideoPreview = ({ videoId }: { videoId: string }) => {
   const videoDetails = useVideoDetails(videoId);
 
   return videoDetails ? (
     <div style={{ display: "flex" }}>
-      <img
-        style={{ width: "200px", borderRadius: "10px", border: "1px solid" }}
-        src={videoDetails.previewUrl}
-        alt="video preview"
-      />
+      <VideoPreviewImage videoDetails={videoDetails} />
 
       <div style={{ paddingLeft: "10px" }}>
-        <div style={{ fontWeight: "bold" }}>{videoDetails.title}</div>
-        <div style={{ color: "#808080" }}>{videoDetails.author}</div>
+        <VideoDescription videoDetails={videoDetails} />
       </div>
     </div>
   ) : (
-    <span>loading...</span>
+    <Loader />
   );
 };
 
