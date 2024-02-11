ae8c0f899f89c183044c3acc377e96100775a650
Anton Balashov <balashov.anton.1983@gmail.com>
2024-02-11 21:27:33 +0200

    [LSP] Reuse VideoPreview component for stream preview

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ type VideoDetails = {
   author: string;
 };
 
+type StreamDetails = VideoDetails & { watching: number };
+
 const loadVideoDetails = (id: string): Promise<VideoDetails> => {
   return new Promise((resolve): void => {
     setTimeout(
@@ -20,11 +22,36 @@ const loadVideoDetails = (id: string): Promise<VideoDetails> => {
   });
 };
 
+const loadStreamDetails = (id: string): Promise<StreamDetails> => {
+  return new Promise((resolve): void => {
+    setTimeout(
+      () =>
+        resolve({
+          previewUrl: "https://i.ytimg.com/vi/gYszgvLdxpI/hqdefault.jpg",
+          title: "Do we really need SOLID?",
+          author: "@NtonBala",
+          watching: 12000,
+        }),
+      500,
+    );
+  });
+};
+
 const useVideoDetails = (videoId: string) => {
   const [videoDetails, setVideoDetails] = useState<VideoDetails>();
 
   useEffect(() => {
     loadVideoDetails(videoId).then((vd) => setVideoDetails(vd));
+  }, [videoId]);
+
+  return videoDetails;
+};
+
+const useStreamDetails = (videoId: string) => {
+  const [videoDetails, setVideoDetails] = useState<StreamDetails>();
+
+  useEffect(() => {
+    loadStreamDetails(videoId).then((vd) => setVideoDetails(vd));
   }, [videoId]);
 
   return videoDetails;
@@ -89,6 +116,11 @@ function App() {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <VideoPreview videoId="testVideo" />
+      <br />
+      <VideoPreview
+        videoId="testStream"
+        videoDetailsGetter={useStreamDetails}
+      />
     </div>
   );
 }
